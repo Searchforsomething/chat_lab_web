2517b2e2e66bb34edb9ee91b55481a0d191c9e47
Коротыкина Анастасия Александровна <103072332+Searchforsomething@users.noreply.github.com>
2025-02-09 21:09:43 +0300

    Update crud.py

--- website/app/crud.py
+++ website/app/crud.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
-from .init import User, Chat
+
 from . import schemas
+from .init import User, Chat
+
 
 def create_chat(db: Session, chat: schemas.ChatCreate, user_id: int):
     db_chat = Chat(name=chat.name, owner_id=user_id)
@@ -9,6 +11,7 @@ def create_chat(db: Session, chat: schemas.ChatCreate, user_id: int):
     db.refresh(db_chat)
     return db_chat
 
+
 def delete_chat(db: Session, chat_id: int):
     chat = db.query(Chat).filter(Chat.id == chat_id).first()
     if chat:
@@ -16,8 +19,10 @@ def delete_chat(db: Session, chat_id: int):
         db.commit()
     return chat
 
+
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
+
 
 def create_user(db: Session, user: schemas.UserCreate):
     db_user = User(email=user.email, hashed_password=user.password)
